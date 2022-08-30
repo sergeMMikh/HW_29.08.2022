a20be98161956a1ad3b1a9862a96b6052fd4c326
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-30 21:24:51 +0100

    added filtration

--- logistic/views.py
+++ logistic/views.py
@@ -8,9 +8,11 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products']
