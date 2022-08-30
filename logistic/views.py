c7991fc0101624e3625c8824ea7784c1c27b1c55
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-30 21:39:07 +0100

    added pagination

--- logistic/views.py
+++ logistic/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -8,11 +11,14 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
-    filterset_fields = ['title', 'description']
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', ]
+    search_fields = ['description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['products']
